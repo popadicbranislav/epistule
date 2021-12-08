8aa02919776771c57dbc43ccf5f53e6bbd909824
branislav.popadic@t-systems.com <branislav.popadic@t-systems.com>
2021-12-08 13:07:12 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    app
+    <Chat></Chat>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Chat from "./components/ChatWrapper.vue";
+</script>
+
+<style>
+#app {
+  font-family: "Roboto", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #222;
+}
+
+* {
+  border: 1px solid #bbb;
+}
+</style>
---
+++ src/components/ChatWrapper.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>chat wrapper</div>
+</template>
+
+<script setup lang="ts">
+//
+</script>
