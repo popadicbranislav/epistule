4935628ad865e9f499af89461123150694b4530d
branislav.popadic@t-systems.com <branislav.popadic@t-systems.com>
2021-12-12 07:19:26 +0100

    Changed send button in chat input

--- src/components/ChatInput.vue
+++ src/components/ChatInput.vue
@@ -1,13 +1,26 @@
 <template>
   <form id="chat-form" @submit.prevent="onSubmitChatInput">
     <input
-      id="chat-input"
+      class="chat-input"
       type="text"
       placeholder="Enter message"
       v-model="state.chat_input"
     />
 
-    <button type="submit">send</button>
+    <button class="chat-submit-btn" type="submit">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24px"
+        viewBox="0 0 24 24"
+        width="24px"
+        fill="currentColor"
+      >
+        <path d="M0 0h24v24H0V0z" fill="none" />
+        <path
+          d="M4.01 6.03l7.51 3.22-7.52-1 .01-2.22m7.5 8.72L4 17.97v-2.22l7.51-1M2.01 3L2 10l15 2-15 2 .01 7L23 12 2.01 3z"
+        />
+      </svg>
+    </button>
   </form>
 </template>
 
@@ -70,7 +83,7 @@ function onSubmitChatInput() {
   box-shadow: 0 -0.2rem 0.8rem #0004;
 }
 
-#chat-input {
+.chat-input {
   flex: 1;
   font-size: 2rem;
 
@@ -78,5 +91,40 @@ function onSubmitChatInput() {
 
   border: none;
   outline: none;
+}
+
+.chat-submit-btn {
+  border: none;
+  margin: 0;
+  padding: 0;
+  width: auto;
+  overflow: visible;
+
+  background: transparent;
+
+  /* inherit font & color from ancestor */
+  color: inherit;
+  font: inherit;
+
+  /* Normalize `line-height`. Cannot be changed from `normal` in Firefox 4+. */
+  line-height: normal;
+
+  /* Corrects font smoothing for webkit */
+  -webkit-font-smoothing: inherit;
+  -moz-osx-font-smoothing: inherit;
+
+  /* Corrects inability to style clickable `input` types in iOS */
+  -webkit-appearance: none;
+
+  /* height: 100%; */
+
+  /* font-size: 1.5rem; */
+
+  color: #aaa;
+  cursor: pointer;
+}
+
+.chat-submit-btn:hover {
+  color: #888;
 }
 </style>
