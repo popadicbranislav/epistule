510ba8c6e551734d6a873631dceaf54d1f4ecdd0
branislav.popadic@t-systems.com <branislav.popadic@t-systems.com>
2021-12-12 07:06:45 +0100

    Updaed chat.ts store with basic structure of message

--- src/store/chat.ts
+++ src/store/chat.ts
@@ -1,9 +1,29 @@
 import { defineStore } from "pinia";
 
-export const useStore = defineStore("chat", {
-  state: () => {
+type Message = {
+  id: string;
+  username: "bot" | string;
+  message: {
+    type: "text" | "buttons";
+    text: string;
+  };
+  time: number;
+};
+
+type ChatStore = {
+  messages: Message[];
+};
+
+export const useChatStore = defineStore("chat", {
+  state: (): ChatStore => {
     return {
-      count: 1,
+      messages: [],
     };
+  },
+
+  actions: {
+    addMessage(msg: Message) {
+      this.messages.push(msg);
+    },
   },
 });
