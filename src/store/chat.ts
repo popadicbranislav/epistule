904bd20943b95f4eccd1596a15f3506d8b18358a
branislav.popadic@t-systems.com <branislav.popadic@t-systems.com>
2021-12-14 20:12:38 +0100

    Updated chat.ts - differentiate between buttons and texts

--- src/store/chat.ts
+++ src/store/chat.ts
@@ -1,17 +1,32 @@
 import { defineStore } from "pinia";
 
-type Message = {
+export type Message = {
   id: string;
   username: "bot" | string;
   message: {
-    type: "text" | "buttons";
+    type: "text";
     text: string;
   };
   time: number;
 };
 
+type Button = {
+  title: string;
+  payload: string;
+};
+
+export type ButtonGroup = {
+  id: string;
+  username: "bot";
+  message: {
+    type: "buttons";
+    buttons: Button[];
+  };
+  time: number;
+};
+
 type ChatStore = {
-  messages: Message[];
+  messages: Array<Message | ButtonGroup>;
 };
 
 export const useChatStore = defineStore("chat", {
@@ -24,6 +39,10 @@ export const useChatStore = defineStore("chat", {
   actions: {
     addMessage(msg: Message) {
       this.messages.push(msg);
+    },
+
+    addButtons(buttons: ButtonGroup) {
+      this.messages.push(buttons);
     },
   },
 });
