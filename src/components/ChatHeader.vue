f230bc79b65fc6a02b817e106586148967e35f60
branislav.popadic@t-systems.com <branislav.popadic@t-systems.com>
2021-12-15 06:42:52 +0100

    Shows sender id and option to reset it

--- src/components/ChatHeader.vue
+++ src/components/ChatHeader.vue
@@ -1,8 +1,32 @@
 <template>
-  <div id="chat-header"><h1>Chat header</h1></div>
+  <div id="chat-header">
+    <span>
+      sender_id:
+      <span class="id" :title="sender_id">
+        {{ sender_id }}
+      </span>
+    </span>
+
+    <button @click="reset">RESET</button>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import { SENDER_ID_KEY_NAME } from "../common/config";
+import { v4 as uuid } from "uuid";
+
+let sender_id = ref(
+  window.localStorage.getItem(SENDER_ID_KEY_NAME) || undefined
+);
+
+console.debug("sender_id", sender_id.value);
+
+function reset() {
+  sender_id.value = uuid();
+  window.localStorage.setItem(SENDER_ID_KEY_NAME, sender_id.value);
+}
+</script>
 
 <style scoped>
 #chat-header {
@@ -15,8 +39,16 @@
   box-shadow: 0 0.2rem 0.8rem #0004;
 }
 
-h1 {
-  font-size: 2.5rem;
-  font-weight: normal;
+span {
+  font-size: 1.5rem;
+
+  white-space: nowrap;
+  overflow-x: hidden;
+  text-overflow: ellipsis;
+}
+
+span.id {
+  background-color: #ededed;
+  padding: 0 0.5rem;
 }
 </style>
